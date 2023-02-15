ba97352c953bd691c8603a03a1af8c22a72c2c24
K9spud LLC <none>
2023-02-15 13:11:29 -0700

    Added support for drag and drop.

---
+++ k9mimedata.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2023, K9spud LLC.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#ifndef K9MIMEDATA_H
+#define K9MIMEDATA_H
+
+#include <QMimeData>
+
+class CompositeView;
+class TabWidget;
+class QDrag;
+class K9MimeData : public QMimeData
+{
+    Q_OBJECT
+public:
+    explicit K9MimeData();
+    ~K9MimeData();
+
+    CompositeView* view;
+    int sourceIndex;
+    TabWidget* sourceTabWidget;
+
+signals:
+
+};
+
+#endif // K9MIMEDATA_H
